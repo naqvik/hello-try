f4e491de68899cca1f1d22f2bc4f44d3c36bd5b9
Karim Naqvi <naqvik@uregina.ca>
2020-11-24 07:49:34 -0600

    Add feature: sine table

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include "cmath"
 
 int main()
 {
     std::cout << "hello world!\n";
+    std::cout << "Angle (rad)    sin(angle)\n";
+    for (int i = 0; i < 10; i++) {
+	float angle = i*.1;
+	std::cout << angle << "\t" << sin(angle) << "\n";
+    }
 }
